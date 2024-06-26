fb296c03a4680f43e37502bdefa7b5cdad1175d8
jperez0630 <jasonscottperez@gmail.com>
2024-06-25 19:40:23 -0700

    updated data

--- sources/marinerds_data/get_data.py
+++ sources/marinerds_data/get_data.py
@@ -195,7 +195,7 @@ def get_mariners_staff_data():
 df_mariners_staff = get_mariners_staff_data()
 
 def get_game_data():
-    data = statcast(start_dt='2024-03-28', end_dt='2024-06-21', team='SEA')
+    data = statcast(start_dt='2024-03-28', end_dt='2024-06-24', team='SEA')
     return data
 
 df_game_data = get_game_data()
